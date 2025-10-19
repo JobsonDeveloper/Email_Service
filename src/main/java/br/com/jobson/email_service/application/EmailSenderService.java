102bc94acc89ee282aa889e57d47cace38da3541
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2025-10-18 22:06:45 -0300

    Starting project

---
+++ src/main/java/br/com/jobson/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package br.com.jobson.email_service.application;
+
+import br.com.jobson.email_service.adapters.EmailSenderGateway;
+import br.com.jobson.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway) {
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
+
+// 23:45
